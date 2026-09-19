4e879ae4a424764273ce2df1139d08668b317382
dinhanh1991 <lovetobelove15@gmail.com>
2026-09-19 23:58:23 +0700

    Standardize controller responsibilities

--- LibraryManagement/controllers/borrow_controller.py
+++ LibraryManagement/controllers/borrow_controller.py
@@ -1,21 +1,16 @@
+from LibraryManagement.controllers.base_controller import BaseController
 from LibraryManagement.views.borrow_view import BorrowView
 
 
-class BorrowController:
-    """Coordinate borrowing workflows and delegate presentation to BorrowView."""
+class BorrowController(BaseController):
+    """Điều phối menu quản lý mượn sách."""
 
     def __init__(self, service, view=None):
-        # Dùng cùng service với các chức năng khác để dữ liệu luôn nhất quán.
-        self.view = view or BorrowView(service)
+        super().__init__(view or BorrowView(service))
 
     def get_actions(self):
-        # Ánh xạ lựa chọn menu mượn sách tới phương thức tương ứng của view.
         return {
             "1": self.view.borrow_book,
             "2": self.view.display_queue,
             "3": self.view.process_next_borrower,
         }
-
-    def run(self):
-        # Controller điều phối vòng lặp; BorrowView chịu trách nhiệm giao diện.
-        self.view.run_menu(self.view.show_menu, self.get_actions())
