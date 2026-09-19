f8e9e39b577d9ee0dec863bad9c6aa349a93ef29
dinhanh1991 <lovetobelove15@gmail.com>
2026-09-19 23:58:18 +0700

    Standardize controller responsibilities

--- LibraryManagement/controllers/book_controller.py
+++ LibraryManagement/controllers/book_controller.py
@@ -1,15 +1,14 @@
+from LibraryManagement.controllers.base_controller import BaseController
 from LibraryManagement.views.book_view import BookView
 
 
-class BookController:
-    """Coordinate the book-management menu and delegate presentation to BookView."""
+class BookController(BaseController):
+    """Điều phối menu quản lý sách."""
 
     def __init__(self, service, view=None):
-        # Nhận dependency từ LibraryController thay vì tự tạo service mới.
-        self.view = view or BookView(service)
+        super().__init__(view or BookView(service))
 
     def get_actions(self):
-        # Controller xử lý điều hướng; các thao tác nhập/xuất vẫn nằm trong BookView.
         return {
             "1": self.view.add_book,
             "2": self.view.delete_book,
@@ -19,7 +18,3 @@ class BookController:
             "6": self.view.search_by_author,
             "7": self.view.search_advanced,
         }
-
-    def run(self):
-        # Tái sử dụng vòng lặp menu chung của BaseView.
-        self.view.run_menu(self.view.show_menu, self.get_actions())
